244d0dd247d33c0a25bbfc0bbbd92b8fec6e50f4
John Hoffer <john@hoff.in>
2021-03-23 15:35:44 -0400

    test windows xml dir

--- package_win.bat
+++ package_win.bat
@@ -1,1 +1,1 @@
-pyinstaller -F --paths $env:CONDA_PREFIX --hidden-import="pkg_resources.py2_warn" --add-data "static;static" --add-data "minerva-story;minerva-story" --add-data "%CONDA_PREFIX%\lib\python3.7\site-packages\xmlschema\schemas;xmlschema\schemas" --icon icon.ico --name minerva_author src/app.py
+pyinstaller -F --paths $env:CONDA_PREFIX --hidden-import="pkg_resources.py2_warn" --add-data "static;static" --add-data "minerva-story;minerva-story" --add-data "%CONDA_PREFIX%\Lib\site-packages\xmlschema\schemas;xmlschema\schemas" --icon icon.ico --name minerva_author src/app.py
