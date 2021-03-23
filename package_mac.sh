579f957467dafcb77dfd5de6057b8e87f86a0952
John Hoffer <john@hoff.in>
2021-03-23 14:42:42 -0400

    add xml schema to pyinstaller bundles

--- package_mac.sh
+++ package_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller -F --paths $CONDA_PREFIX --add-data "static:static" --add-data "minerva-story:minerva-story" src/app.py
+pyinstaller -F --paths $CONDA_PREFIX --add-data "static:static" --add-data "minerva-story:minerva-story" --add-data "$CONDA_PREFIX/lib/python3.7/site-packages/xmlschema/schemas:xmlschema/schemas" src/app.py
