e191a7f9cb424df3e9bdf929e2edf59c69a4cae8
John Hoffer <john@hoff.in>
2021-09-20 11:48:11 -0400

    attempt to fix altair dependency issue

--- package_mac.sh
+++ package_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller -F --paths $CONDA_PREFIX --add-data "static:static" --add-data "minerva-story:minerva-story" --add-data "$CONDA_PREFIX/lib/python3.8/site-packages/xmlschema/schemas:xmlschema/schemas" src/app.py
+pyinstaller -F --paths $CONDA_PREFIX --add-data "static:static" --add-data "minerva-story:minerva-story" --add-data "$CONDA_PREFIX/lib/python3.8/site-packages/altair/vegalite/v4/schema:altair/vegalite/v4/schema" --add-data "$CONDA_PREFIX/lib/python3.8/site-packages/xmlschema/schemas:xmlschema/schemas" src/app.py
