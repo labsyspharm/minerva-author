eb692d3b4e0505dc78cd1ec03cb3d654e3cec825
John Hoffer <john@hoff.in>
2022-10-17 15:24:24 -0400

    thrashing every 1 second

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 import numpy
+import multiprocessing
 from flask import Flask
 from waitress import serve
 
+lock_1 = multiprocessing.Lock()
+multiprocessing.freeze_support()
 app = Flask(__name__)
 
 if __name__ == "__main__":
