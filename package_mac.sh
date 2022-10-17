d760ec3be4c80df475cd5ce02dc985351a858494
John Hoffer <john@hoff.in>
2022-10-17 15:20:00 -0400

    reduce the speed of disk thrashing

--- package_mac.sh
+++ package_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller -F --paths $CONDA_PREFIX --add-data "static:static" --add-data "minerva-story:minerva-story" --add-data "$CONDA_PREFIX/lib/python3.10/site-packages/altair/vegalite/v4/schema:altair/vegalite/v4/schema" --add-data "$CONDA_PREFIX/lib/python3.10/site-packages/xmlschema/schemas:xmlschema/schemas" --add-data "$CONDA_PREFIX/lib/python3.10/site-packages/ome_types:ome_types" src/app.py
+pyinstaller -F --add-data "static:static" --add-data "minerva-story:minerva-story" --add-data "$CONDA_PREFIX/lib/python3.10/site-packages/altair/vegalite/v4/schema:altair/vegalite/v4/schema" --add-data "$CONDA_PREFIX/lib/python3.10/site-packages/xmlschema/schemas:xmlschema/schemas" --add-data "$CONDA_PREFIX/lib/python3.10/site-packages/ome_types:ome_types" src/app.py
