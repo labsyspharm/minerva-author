d760570a18b1acfd123973f3166900f3dc03765e
Jeremy Muhlich <jmuhlich@bitflood.org>
2024-03-01 13:54:46 -0500

    Add pyinstaller tweak for xsdata_pydantic_basemodel

--- package_mac.sh
+++ package_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller -F --add-data "static:static" --add-data "minerva-story:minerva-story" --collect-all altair --collect-all xmlschema --collect-all ome_types src/app.py
+pyinstaller -F --add-data "static:static" --add-data "minerva-story:minerva-story" --collect-all altair --collect-all xmlschema --collect-all ome_types --collect-submodules xsdata_pydantic_basemodel src/app.py
