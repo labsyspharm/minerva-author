3519c4da3d6765e5ac187d1de1b6db8969e6aa2d
John Hoffer <john@hoff.in>
2025-11-20 14:57:22 -0500

    add imagecodecs.jpeg8_decode to package_mac

--- package_mac.sh
+++ package_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller -F --add-data "static:static" --add-data "minerva-story:minerva-story" --collect-all altair --collect-all xmlschema --collect-all ome_types --collect-submodules xsdata_pydantic_basemodel --hidden-import "imagecodecs._shared" --hidden-import "imagecodecs._imcd" src/app.py
+pyinstaller -F --add-data "static:static" --add-data "minerva-story:minerva-story" --collect-all altair --collect-all xmlschema --collect-all ome_types --collect-submodules xsdata_pydantic_basemodel --hidden-import "imagecodecs._shared" --hidden-import "imagecodecs._imcd" --hidden-import "imagecodecs.jpeg8_decode" src/app.py
